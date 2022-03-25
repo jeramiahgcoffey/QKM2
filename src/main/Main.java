cb56f42718e51fecc201a99671dc0234e9afad05
jeramiahgcoffey <jerrygcoffey@gmail.com>
2022-03-25 14:58:34 -0500

    Inital commit

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
+        stage.setTitle("Main Form");
+        stage.setScene(new Scene(root, 1000, 500));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
