0777e658ab67c3ee6e7e3def673711762025b4ce
jeramiahgcoffey <jerrygcoffey@gmail.com>
2022-03-28 15:40:43 -0500

    Product class

---
+++ src/model/Product.java
@@ -0,0 +1,83 @@
+package model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Product {
+    private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private int min;
+    private int max;
+
+    public Product(ObservableList<Part> associatedParts, int id, String name, double price, int stock, int min, int max) {
+        this.associatedParts = associatedParts;
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void addAssociatedPart(Part part) {
+        associatedParts.add(part);
+    }
+
+//    Implement deleteAssociatedPart
+
+
+    public ObservableList<Part> getAssociatedParts() {
+        return associatedParts;
+    }
+}
